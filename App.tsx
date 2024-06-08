3392f225a7d473d137f53ad2859e4ba9670e25c1
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-06-08 15:52:48 -0300

    feat(p2-pdmt): botão gerar e testes de api
    - Testes com o primeiro botão criado

--- App.tsx
+++ App.tsx
@@ -1,23 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { TheCatAPI } from "@thatapicompany/thecatapi"
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, FlatList, Image } from 'react-native';
+
+type Gatos = {
+  id: string;
+  url: string;
+}
 
 export default function App() {
   const { API_KEY } = process.env;
   const theCatAPI = new TheCatAPI(API_KEY || '');
+  const [gatos, setGatos] = useState<Gatos[]>([]);
 
-  theCatAPI.images.searchImages({
-    limit: 5,
-  }).then((images) => {
-    console.log(images);
-  }).catch((error) => {
-    console.error(error);
-  });
+  const gerarGato = async () => {
+    const gato = await theCatAPI.images.searchImages({
+      limit: 5,
+    });
 
+    setGatos(gato);
+
+    console.log(gato);
+    return gato;
+  }
 
   return (
     <View style={styles.container}>
       <Text>Gerador de fotos de gatos</Text>
+      <Pressable
+        style={styles.button}
+        onPress={gerarGato}
+      >
+        <Text>Gerar</Text>
+      </Pressable>
+
+      <FlatList
+        keyExtractor={(item) => item.id!}
+        data={gatos}
+        style={styles.list}
+        renderItem={gatos => (
+        <View>
+          <Image
+            source={{ uri: gatos.item.url }}
+            style={{ width: '100%', height: 200 }}
+          />
+        </View>
+        )}
+      >
+
+      </FlatList>
     </View>
   );
 }
@@ -28,5 +58,27 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingVertical: 40,
   },
+
+  button: {
+    borderColor: 'black',
+    borderWidth: 3,
+    borderRadius: 4,
+    marginTop: 15,
+    width: 80,
+    height: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontFamily: 'arial',
+    marginBottom: 20,
+  },
+
+  list: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 4,
+    width: '80%',
+  }
+
 });
