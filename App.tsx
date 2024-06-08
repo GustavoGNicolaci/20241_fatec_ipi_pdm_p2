aedd6c2d1eae6a6700f7acee14a87727b44a2457
Gustavo Nicolaci <gustavo_nicolaci@hotmail.com>
2024-06-08 14:54:57 -0300

    feat(p2-pdmt): adição de depedências e API
    - adiciona .env
    - adiciona API e realiza o teste no console

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
+import { TheCatAPI } from "@thatapicompany/thecatapi"
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const { API_KEY } = process.env;
+  const theCatAPI = new TheCatAPI(API_KEY || '');
+
+  theCatAPI.images.searchImages({
+    limit: 5,
+  }).then((images) => {
+    console.log(images);
+  }).catch((error) => {
+    console.error(error);
+  });
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Gerador de fotos de gatos</Text>
     </View>
   );
 }
